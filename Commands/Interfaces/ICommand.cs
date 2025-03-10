c34a8321b4973b37d910fb5000d59cd8dc12c247
SCOTT MUMBY <smumby2003@gmail.com>
2025-03-10 22:01:17 +0000

    Add command interfaces

---
+++ Commands/Interfaces/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MumboDB.Commands.Interfaces
+{
+    public interface ICommand
+    {
+        //All Commands Must Have an Execute Method
+        public void Execute();
+    }
+}
---
+++ Commands/Interfaces/ICommandWithParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MumboDB.Commands.Interfaces
+{
+    public interface ICommandWithParams : ICommand
+    {
+        //Commands with params contain this string array
+        public List<string> commandParams { get; set; }
+    }
+}
