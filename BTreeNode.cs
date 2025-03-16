fb17d97cff12fd8f58d5cb226588b464d3f5a8ea
SCOTT MUMBY <smumby2003@gmail.com>
2025-03-16 21:14:08 +0000

    Added B-Tree Node

---
+++ BTreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MumboDB
+{
+    public class BTreeNode
+    {
+        public int[] key = new int[3];
+        public List<BTreeNode> childNodes = new List<BTreeNode>();
+        public bool isLeaf;
+    }
+}
